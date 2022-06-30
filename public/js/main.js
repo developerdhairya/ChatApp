44a1759e5fd54214ca89d08ff2712175ddf76384
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-06-30 20:33:11 +0530

    cpm-2

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,57 @@
 const chatForm = document.getElementById('chat-form');
-
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
-socket.on('welcomeMessage', message => {
-    console.log(message);
+
+
+
+
+socket.on('message', message => {
+    displayMessage(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    const textField = e.target.elements.msg;
+
     //get message from the text field
-    const message = e.target.elements.msg.value;
+    const message = textField.value;
 
     // send message to the server
     socket.emit('chatMessage', message);
 
+    textField.value = '';
+
+
 });
 
+function displayMessage(message) {
+    const element = document.createElement('div');
+    element.classList.add('message');
+    element.innerHTML = `
+
+            <p class="meta">${message.username} <span>${message.time}</span></p>
+            <p class="text">
+                ${message.text}
+            </p>
+
+    `;
+
+    document.querySelector('.chat-messages').append(element);
+
+}
+
+function getUsernameAndRoom(key) {
+    const url = window.location.href;
+    const params = new URL(url).searchParams;
+    const username = params.get('username');
+    const password = params.get('password');
+
+    return {
+        username,
+        password
+    }
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./public/utils/messages')
 
 const app = express();
 
@@ -10,25 +11,29 @@ const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = "Room Monitor";
+
 //Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection...');
 
-    //Welcome new user
-    socket.emit("welcomeMessage","welcome! lets discuss on global goals");
+    socket.on('joinRoom', ({ username, room }) => {
+        //Welcome new user
+        socket.emit("message", formatMessage(botName, 'Welcome to GlobalGoals Chat'));
 
-    //Broadcasts when a user connects
-    socket.broadcast.emit('message','A user has joined the chat');
-
-
-    //Runs when client disconnect
-    socket.on('disconnect',()=>{
-        io.emit('message','A user has left the chat');
+        //Broadcasts when a user connects
+        socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
     });
 
+
+
     //listen for chat message
-    socket.on('chatMessage',msg => {
-        console.log(msg);
+    socket.on('chatMessage', msg => {
+        io.emit("message", formatMessage('USER', msg));
+    });
+
+    //Runs when client disconnect
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
 });
